1264d9f6d45a3510f61cd0735294fbc7d224f1ee
shubhamdev <shubhamsutar5799@gmail.com>
2026-05-17 19:46:37 +0530

    update title

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from scrapers.trek_scraper import scrape_events
 import pandas as pd
 
-st.title("Web Scraping with Streamlit")
+st.title("Web Scraping")
 st.write("My first Streamlit app")
 
 events = scrape_events()
